43a10c6727ada73f7de2288cdab931d9bbcecc8f
Dmitriy Zaitsev <dmitriy_zaitsev@mail.ru>
2024-10-26 22:47:07 +0300

    Applied patches up to 4_7_create_inmemory_test_ctx.patch

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,7 +1,14 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.util.Assert;
+
 public abstract class AbstractBaseEntity {
+    public static final int START_SEQ = 100000;
+
     protected Integer id;
+
+    protected AbstractBaseEntity() {
+    }
 
     protected AbstractBaseEntity(Integer id) {
         this.id = id;
@@ -15,6 +22,11 @@ public abstract class AbstractBaseEntity {
         return id;
     }
 
+    public int id() {
+        Assert.notNull(id, "Entity must have id");
+        return id;
+    }
+
     public boolean isNew() {
         return this.id == null;
     }
@@ -22,5 +34,22 @@ public abstract class AbstractBaseEntity {
     @Override
     public String toString() {
         return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
     }
 }
--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryBaseRepository.java
@@ -4,16 +4,20 @@ import ru.javawebinar.topjava.model.AbstractBaseEntity;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
 public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
 
-    static final AtomicInteger counter = new AtomicInteger(0);
+    static final AtomicInteger counter = new AtomicInteger(START_SEQ);
 
     final Map<Integer, T> map = new ConcurrentHashMap<>();
 
     public T save(T entity) {
+        Objects.requireNonNull(entity, "Entity must not be null");
         if (entity.isNew()) {
             entity.setId(counter.incrementAndGet());
             map.put(entity.getId(), entity);
@@ -35,6 +39,7 @@ public class InMemoryBaseRepository<T extends AbstractBaseEntity> {
     }
 
     void put(T entity) {
-        map.put(entity.getId(), entity);
+        Objects.requireNonNull(entity, "Entity must not be null");
+        map.put(entity.id(), entity);
     }
 }
